5d67cd094e96bdc406d9aded9ae95531b6dcd981
Chakshita Jaswal <chakshitajaswal2106@gmail.com>
2025-05-09 18:46:38 +0530

    new

--- practicequestions.c
+++ practicequestions.c
@@ -413,3 +413,21 @@
 //     return 0;
 // }
 
+
+
+#include <stdio.h>
+int main() {
+    int i , a[10], n, pos=0,neg=0;
+    for (int i = 0;i<10;i++) {
+        scanf("%d",&a[i]);
+    
+    if (a[i]<0) {
+        neg++;
+    }
+    else {
+        pos++;
+    }
+    }
+    printf("%d\n%d",pos,neg);
+    return 0;
+}
