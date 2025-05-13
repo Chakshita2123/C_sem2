1f819bc67d17ca1d6a80c740a2684ac0485589b7
Chakshita Jaswal <chakshitajaswal2106@gmail.com>
2025-05-13 11:26:38 +0530

    new

--- practicequestions.c
+++ practicequestions.c
@@ -1036,3 +1036,36 @@
 //     printf("%d\n",**pptr);
 //     return 0;
 // }
+
+
+
+
+// POINTERS IN FUNCTION CALL
+// 1. call by value - we pass value of variable as argument
+// 2. call by reference - we pass address of variable as argument
+
+
+// #include <stdio.h>
+// void square(int n);
+// void _square(int* n);
+
+// int main() {
+//     int number = 4;
+//     square(number);
+//     printf("Number = %d\n",number);
+
+//     _square(&number);
+//     printf("Number = %d\n",number);
+//     return 0;
+// }
+// // call by value
+// void square (int n) {
+//     n = n*n;
+//     printf("Square = %d\n",n);
+// }
+// // call by reference
+// void _square(int* n){
+//     *n = *n * *n;
+//     printf("Square = %d\n",*n);
+// }
+
