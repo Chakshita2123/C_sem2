a8a680cdd69496d119e09694c191e85093c047b9
Chakshita Jaswal <chakshitajaswal2106@gmail.com>
2025-05-13 10:47:21 +0530

    new

--- practicequestions.c
+++ practicequestions.c
@@ -927,3 +927,28 @@
 //     }
 //     return 0;
 // }
+
+
+// fibonacci series
+// #include <stdio.h>
+// int fibonacciseries (int n) {
+//     if (n <= 0){
+//         printf("Input should be positive integer.");
+//     }
+//     else if (n == 1) {
+//         return 0;
+//     }
+//     else if (n == 2) {
+//         return 1;
+//     }
+//     else {
+//         return fibonacciseries(n-1) + fibonacciseries(n-2);
+//     }
+// }
+// int main() {
+//     int n;
+//     printf("Enter the number of terms: ");
+//     scanf("%d",&n);
+//     printf("Fibonacci series up to %d terms is: ",n);
+//     return fibonacciseries(n);
+// }
