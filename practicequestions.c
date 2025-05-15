7c6b5fd8c79fdddca251465c1e31867c3451febf
Chakshita Jaswal <chakshitajaswal2106@gmail.com>
2025-05-15 11:03:34 +0530

    new

--- practicequestions.c
+++ practicequestions.c
@@ -1310,3 +1310,27 @@
     
 //     return 0;
 // }
+
+
+// Ques. Write a program to store the first n fibonacci numbers
+// #include <stdio.h>
+// int main() {
+//     int n;
+//     printf("Enter the number of Fibonacci numbers to generate: ");
+//     scanf("%d", &n);
+    
+//     int fib[n];
+//     fib[0] = 0;
+//     fib[1] = 1;
+    
+//     for (int i = 2; i < n; i++) {
+//         fib[i] = fib[i - 1] + fib[i - 2];
+//     }
+    
+//     printf("Fibonacci series:\n");
+//     for (int i = 0; i < n; i++) {
+//         printf("%d ", fib[i]);
+//     }
+    
+//     return 0;
+// }
