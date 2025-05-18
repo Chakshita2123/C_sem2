9e016dfc28dd78e9eea06bcdcd0d84732f329f84
Chakshita Jaswal <chakshitajaswal2106@gmail.com>
2025-05-18 18:17:16 +0530

    transpose

--- practicequestions.c
+++ practicequestions.c
@@ -2010,3 +2010,34 @@
 //     }
 //     return 0;
 // }
+
+
+
+// Transpose of a matrix
+// #include <stdio.h>
+// int main() {
+//     int i,j,rows,cols;
+//     printf("Enter the number of rows and columns: ");
+//     scanf("%d %d",&rows, &cols);
+
+//     int matrix[100][100], transpose[100][100];
+//     printf("Enter the elements of the matrix: \n");
+//     for (i=0;i<rows;i++) {
+//         for (j=0;j<cols;j++){
+//             scanf("%d",&matrix[i][j]);
+//         }
+//     }
+//     for (i=0;i<rows;i++) {
+//         for (j=0;j<cols;j++) {
+//             transpose[j][i] = matrix[i][j];
+//         }
+//     }
+//     printf("Transpose of matrix: \n");
+//     for (i=0;i<cols;i++) {
+//         for (j=0;j<rows;j++) {
+//             printf("%d ",transpose[i][j]);
+//         }
+//         printf("\n");
+//     }
+//     return 0;
+// }
